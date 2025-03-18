b5f6e551121304f440192a0070f503e7e111d73b
Kalam-shadow <rankholder007@gmail.com>
2025-03-18 22:02:54 +0530

    ak - fcm 1

--- backend/api/firebase.py
+++ backend/api/firebase.py
@@ -1,11 +1,35 @@
 import firebase_admin
-from firebase_admin import credentials, firestore
+from firebase_admin import credentials, firestore, messaging
 
 # Load Firebase credentials from a service account JSON file
-cred = credentials.Certificate(r"/run/media/adithya/Studies and Related/NurseEaseAdminWeb/backend/serviceAccountKey.json")  # Replace with your actual file path
+cred = credentials.Certificate(r"/NurseEaseAdminWeb/backend/serviceAccountKey.json")  # Replace with your actual file path
 firebase_admin.initialize_app(cred)
 
 def get_firestore():
     """Returns Firestore database client."""
     return firestore.client()
+
+def send_fcm_notification(fcm_token, title, body):
+    """Sends an FCM notification to the specified token."""
+    message = messaging.Message(
+        notification=messaging.Notification(
+            title=title,
+            body=body,
+        ),
+        token=fcm_token,
+        android=messaging.AndroidConfig(
+            notification=messaging.AndroidNotification(
+                sound="default",
+            ),
+        ),
+        apns=messaging.APNSConfig(
+            payload=messaging.APNSPayload(
+                aps=messaging.Aps(
+                    sound="default",
+                ),
+            ),
+        ),
+    )
+    response = messaging.send(message)
+    print(f"Successfully sent message: {response}")
 
